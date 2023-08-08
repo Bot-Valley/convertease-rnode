2cebc3633dad00af5c6971c1984fdd08a141f27b
editid0 <66117959+editid0@users.noreply.github.com>
2023-08-08 19:10:56 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,55 @@ use converter_buddy::format::Format;
 use rocket::fs::NamedFile;
 use rocket::response::status;
 use filename::file_name;
-use log::{info, warn};
+use std::fs;
+use either::*;
 
 #[get("/")]
 fn index() -> &'static str {
-    "Hello, world!"
+    "NO FUCK OFF"
 }
-#[post("/<filename>", format = "image/png", data = "<file>")]
-async fn upload(mut file: TempFile<'_>, filename: String) -> NamedFile {
-    !info(target: "convertRequest", "File upload"); 
+#[post("/<filename>/<format>", data = "<file>")]
+async fn upload(mut file: TempFile<'_>, filename: String, format: String) -> Either<NamedFile,String> {
     file.persist_to(&filename).await;
-    let target_format = Format::Jpeg;
+    let target_format;
+    if format == "jpeg" {
+        target_format = Format::Jpeg;
+    } else if format == "png" {
+        target_format = Format::Png;
+    } else if format == "webp" {
+        target_format = Format::WebP;
+    } else if format == "tiff" {
+        target_format = Format::Tiff;
+    } else if format == "bmp" {
+        target_format = Format::Bmp;
+    } else if format == "pdf" {
+        target_format = Format::Pdf;
+    } else if format == "svg" {
+        target_format = Format::Svg;
+    } else {
+        target_format = Format::Png;
+    }
     let file = ConvertibleFile::new(&filename);
     let format = file.format().expect("No format found");
     // println!("Conversion successful for {:?}", file_name(&a).unwrap())
     let mut fname = String::new();
+    let mut failed = false;
+    let mut error = false;
     match file.convert(target_format) {
         Ok(a) => fname = file_name(&a).unwrap().display().to_string(),
-        Err(e) => println!("Conversion failed: {:?}", e),
+        Err(e) => {
+            failed = true;
+            error = true;
+        },
+    }
+    if error {
+        return Right("Error".to_string());
     }
     println!("Converted to {}", fname);
     let nfr = NamedFile::open(&fname).await.unwrap();
-    nfr
+    fs::remove_file(&fname);
+    fs::remove_file(&filename);
+    Left(nfr)
 }
 
 #[launch]
