0c28d78a11f6073765ba0ccf8754e2b681ad7eff
editid0 <66117959+editid0@users.noreply.github.com>
2023-08-08 20:16:07 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use converter_buddy::io::ConvertibleFile;
 use converter_buddy::format::Format;
 #[macro_use] extern crate rocket;
 use rocket::fs::NamedFile;
-use rocket::response::status;
 use filename::file_name;
 use std::fs;
 use either::*;
@@ -14,7 +13,12 @@ fn index() -> &'static str {
 }
 #[post("/<filename>/<format>", data = "<file>")]
 async fn upload(mut file: TempFile<'_>, filename: String, format: String) -> Either<NamedFile,String> {
-    file.persist_to(&filename).await;
+    match file.persist_to(&filename).await {
+        Ok(_) => (),
+        Err(_e) => {
+            return Right("Error".to_string());
+        }
+    }
     let target_format;
     if format == "jpeg" {
         target_format = Format::Jpeg;
@@ -34,15 +38,13 @@ async fn upload(mut file: TempFile<'_>, filename: String, format: String) -> Eit
         target_format = Format::Png;
     }
     let file = ConvertibleFile::new(&filename);
-    let format = file.format().expect("No format found");
+    let _ = file.format().expect("No format found");
     // println!("Conversion successful for {:?}", file_name(&a).unwrap())
     let mut fname = String::new();
-    let mut failed = false;
     let mut error = false;
     match file.convert(target_format) {
         Ok(a) => fname = file_name(&a).unwrap().display().to_string(),
-        Err(e) => {
-            failed = true;
+        Err(_e) => {
             error = true;
         },
     }
@@ -51,8 +53,8 @@ async fn upload(mut file: TempFile<'_>, filename: String, format: String) -> Eit
     }
     println!("Converted to {}", fname);
     let nfr = NamedFile::open(&fname).await.unwrap();
-    fs::remove_file(&fname);
-    fs::remove_file(&filename);
+    let _ = fs::remove_file(&fname);
+    let _ = fs::remove_file(&filename);
     Left(nfr)
 }
 
